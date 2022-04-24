31e1cb0ac4724db5cf5c6439e927b313e6cea126
yuliavalentine <89343534+yuliavalentine@users.noreply.github.com>
2022-04-24 20:15:48 +0300

    Create day-07.js

---
+++ day-07.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/55e7650c8d894146be000095/train/javascript
+
+function validateMessage(msg) {
+  if (msg === null) {
+    throw new ReferenceError("Message is null!");
+  }
+  if (typeof msg !== "string") {
+    throw new TypeError(
+      `Message should be of type string but was of type ${typeof msg}!`
+    );
+  }
+  if (msg.length > 255 || msg.length === 0) {
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+  }
+  if (msg.match(new RegExp(/<[^>]+>/g))) {
+    return false;
+  }
+  return true;
+}
