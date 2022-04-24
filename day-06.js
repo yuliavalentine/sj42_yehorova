cf16f65d97d9e5b7d58662b4b860da81c5e7d096
yuliavalentine <89343534+yuliavalentine@users.noreply.github.com>
2022-04-24 18:59:13 +0300

    Create day-06.js

---
+++ day-06.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/maximum-multiple/train/javascript
+
+function maxMultiple(divisor, bound) {
+  return bound - (bound % divisor);
+}
+
+// https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+
+function hero(bullets, dragons) {
+  return bullets / dragons >= 2 ? true : false;
+}
+
+// https://www.codewars.com/kata/59441520102eaa25260000bf
+
+function unusualFive() {
+  return "abcdg".length;
+}
