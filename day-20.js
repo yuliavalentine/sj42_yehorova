e13949f91a92b216de67109e0b73b6e64f0eecf7
yuliavalentine <89343534+yuliavalentine@users.noreply.github.com>
2022-05-26 16:04:07 +0300

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/573bca07dffc1aa693000139
+const regex=/^-{0,1}9.*0{4,}$/
+
+//https://www.codewars.com/kata/573d11c48b97c0ad970002d4
+
+const regex=/(https?:\/\/[a-zA-Z0-9.]{1,}\.(net|com))+/gi
