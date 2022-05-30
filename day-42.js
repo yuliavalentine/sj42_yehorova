0237ebed03f9c94d4726493cea5fb62f14d7cee8
yuliavalentine <89343534+yuliavalentine@users.noreply.github.com>
2022-05-30 22:57:17 +0300

    Update day-42.js

--- day-42.js
+++ day-42.js
@@ -4,4 +4,19 @@ function scramble(str1, str2) {
   return [...str2].every(a => str2.split(a).length <= str1.split(a).length);
 }
 
+//https://leetcode.com/problems/two-sum/
+
+const twoSum = function(nums, target) {
+const map = {};
+  nums.forEach( (nuum, i) => map[num] = i);
+  for (let i = 0; i< nums.length; i++) {
+    const pairIdx = map[target - nums[i]];
+    if (pairIdx !== undefined && pairIdx !== i) {
+      return [i, pairIdx];
+    }
+    //map[nums[i]] = i
+}
+return [-1; -1];
+
+
 
