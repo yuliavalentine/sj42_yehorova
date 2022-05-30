177b2527b1b369912a0537532280c6334f71639d
yuliavalentine <89343534+yuliavalentine@users.noreply.github.com>
2022-05-30 23:34:03 +0300

    Update day-18.js

--- day-18.js
+++ day-18.js
@@ -45,9 +45,9 @@ function blackAndWhite(arr) {
 
 //https://www.codewars.com/kata/5735956413c2054a680009ec
 function rndCode() {
-  var p1 = "ABCDEFGHIJKLM";
-  var p2 = "0123456789";
-  var p3 = "~!@#$%^&*";
+  let p1 = "ABCDEFGHIJKLM";
+  let p2 = "0123456789";
+  let p3 = "~!@#$%^&*";
   return [p1, p1, p2, p2, p2, p2, p3, p3]
     .map((v) => v[~~(Math.random() * v.length)])
     .join("");
