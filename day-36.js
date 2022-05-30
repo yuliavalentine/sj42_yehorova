8849e0b5e9f943d651ca0b7996beac95f30f971c
yuliavalentine <89343534+yuliavalentine@users.noreply.github.com>
2022-05-30 19:37:38 +0300

    Create day-36.js

---
+++ day-36.js
@@ -0,0 +1,25 @@
+//
+function submitOrder(user) {
+  // let shoppingCart;
+  // let zipCode;
+  // let shippingRate;
+  // let orderSuccessful;
+  
+  return Promise
+    .all([
+      // Get the current user's shopping cart
+      OrderAPI.getShoppingCartAsync(user),
+      // Also look up the ZIP code from their profile
+      CustomerAPI.getProfileAsync(user)
+    ])
+    .then(([ shoppingCart, profile ]) => {
+      // Calculate the shipping fees
+      const shippingRate = calculateShipping(shoppingCart, profile.zipCode);
+      
+      // Submit the order
+      return OrderAPI.placeOrderAsync(shoppingCart, shippingRate)
+    })
+    .then((orderSuccessful) => {
+      console.log(`Your order ${orderSuccessful? "was" : "was NOT"} placed successfully`);
+    });
+}
